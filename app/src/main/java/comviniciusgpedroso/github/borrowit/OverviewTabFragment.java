5b97db563c737de2f9ef9817b46eeb39f54729de
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2018-12-28 20:05:24 -0200

    Init Overview Tab Fragment

---
+++ app/src/main/java/comviniciusgpedroso/github/borrowit/OverviewTabFragment.java
@@ -0,0 +1,29 @@
+package comviniciusgpedroso.github.borrowit;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OverviewTabFragment extends Fragment {
+
+
+    public OverviewTabFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_overview_tab, container, false);
+    }
+
+}
