916fe0e93e60f6e506526caf5ca8ea6c1acfe3d8
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2018-12-31 04:36:40 -0200

    Added todo for recycler view

--- app/src/main/java/comviniciusgpedroso/github/borrowit/ItemCardAdapter.java
+++ app/src/main/java/comviniciusgpedroso/github/borrowit/ItemCardAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
  * Created by endsieg on 31/12/18.
  */
 
+// TODO Finish the RecyclerView and ItemCardHolder class
 public class ItemCardAdapter extends
         RecyclerView.Adapter<ItemCardAdapter.ItemCardHolder> {
     @Override
