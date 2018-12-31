72ee04599a4d0c6252e3c51dcd797f3fc900f8be
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2018-12-31 04:36:01 -0200

    Added design to the pay tab

---
+++ app/src/main/java/comviniciusgpedroso/github/borrowit/ItemCardAdapter.java
@@ -0,0 +1,30 @@
+package comviniciusgpedroso.github.borrowit;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by endsieg on 31/12/18.
+ */
+
+public class ItemCardAdapter extends
+        RecyclerView.Adapter<ItemCardAdapter.ItemCardHolder> {
+    @Override
+    public ItemCardAdapter.ItemCardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemCardAdapter.ItemCardHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ItemCardHolder {
+
+    }
+}
