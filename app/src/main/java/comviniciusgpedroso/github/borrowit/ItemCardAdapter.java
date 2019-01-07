e5e095c73f26fb398a9f9600c7994fc243edaff1
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2019-01-06 22:33:27 -0200

    DONE // TODO Finish the RecyclerView and ItemCardHolder class DONE // TODO Fix layout inflater Reduced the layouts for card items

--- app/src/main/java/comviniciusgpedroso/github/borrowit/ItemCardAdapter.java
+++ app/src/main/java/comviniciusgpedroso/github/borrowit/ItemCardAdapter.java
@@ -2,15 +2,12 @@ package comviniciusgpedroso.github.borrowit;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,41 +16,35 @@ import java.util.List;
  * Created by endsieg on 31/12/18.
  */
 
-// TODO Finish the RecyclerView and ItemCardHolder class
-    //https://codelabs.developers.google.com/codelabs/android-training-create-recycler-view/index.html?index=..%2F..%2Fandroid-training#3
+
+//https://codelabs.developers.google.com/codelabs/android-training-create-recycler-view/index.html?index=..%2F..%2Fandroid-training#3
 public class ItemCardAdapter extends
         RecyclerView.Adapter<ItemCardAdapter.ItemCardHolder> {
 
     private final LayoutInflater mInflater;
     private ArrayList<Item> mItemArrayList;
-    private boolean mIsToReceive = false; // TODO(1) FIX layout inflater
-    private Context mContext;
     private ViewGroup pr;
 
     public ItemCardAdapter(Context context) {
-        mContext = context;
         mInflater = LayoutInflater.from(context);
     }
 
     @Override
     public ItemCardAdapter.ItemCardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v;
-        // Chooses the layout
         pr = parent;
-        if(mIsToReceive) {
-            v = mInflater.inflate(R.layout.receive_item, parent, false);
-            // v = LayoutInflater.from(parent.getContext()).inflate(R.layout.receive_item, parent, false);
-        } else {
-            v = mInflater.inflate(R.layout.pay_item, parent, false);
-            // v = LayoutInflater.from(parent.getContext()).inflate(R.layout.pay_item, parent, false);
-        }
+        v = mInflater.inflate(R.layout.card_item, parent, false);
         return new ItemCardHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ItemCardHolder holder, int position) {
         Item currentItem = mItemArrayList.get(position);
-
+        // Changes text color from red to green
+        if(currentItem.isToReceive()) {
+            holder.mValue.setTextColor(pr.getResources().getColor(R.color
+                    .colorPrimary));
+        }
         String valueWithCurrencySymbol = "$" + currentItem.getValue(); // TODO Add currency here too
         holder.mValue.setText(valueWithCurrencySymbol);
         holder.mContact.setText(currentItem.getContact());
@@ -96,12 +87,7 @@ public class ItemCardAdapter extends
         public void onClick(View view) {
             Item currentItem = mItemArrayList.get(getAdapterPosition());
             Intent detailIntent;
-            if(mIsToReceive) {
-                detailIntent = new Intent(pr.getContext(), ReceiveDetailActivity.class);
-
-            } else {
-                detailIntent = new Intent(pr.getContext(), PayDetailActivity.class);
-            }
+            detailIntent = new Intent(pr.getContext(), PayDetailActivity.class);
 
             detailIntent.putExtra("amount", currentItem.getAmount());
             detailIntent.putExtra("contact", currentItem.getContact());
