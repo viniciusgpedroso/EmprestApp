bcac4f743f6ffb90cf8d9b38fe1cbaf977a4eb73
endsieg-Inspiron-7460 <vpedroso.ee@gmail.com>
2019-01-11 22:28:18 -0200

    Add ideas for item detail activity

--- app/src/main/java/comviniciusgpedroso/github/borrowit/PayDetailActivity.java
+++ app/src/main/java/comviniciusgpedroso/github/borrowit/PayDetailActivity.java
@@ -3,6 +3,7 @@ package comviniciusgpedroso.github.borrowit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//TODO FIX NAME
 //TODO Add arch progress bar, borrow and due dated in the extremities and amount in the middle
 public class PayDetailActivity extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class PayDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_detail);
     }
+
+    // TODO Add multiple action fab with edit, mark as done and delete
 }
